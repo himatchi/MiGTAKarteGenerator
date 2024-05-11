d51a4881747d563c4467927f815c43b9a179f222
himatchi <deburisu@gmail.com>
2024-05-11 14:17:43 +0900

    バグフィックスのテストコミット

--- docs/js/WantedFinder.js
+++ docs/js/WantedFinder.js
@@ -218,6 +218,7 @@ function refreshWanted(newRawData, oldRawData){
       const normalizedWantedName = normalizeString(wanted.name);
       const wantedcreatedAt = new Date(wanted.createdAt);
       if(normalizedCatchedWantedText.includes(normalizedWantedName) && catchedWantedcreatedAt > wantedcreatedAt){
+        console.log(wanted);
         wanted.isActive = false;
       }
     });
